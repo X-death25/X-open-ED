9385c9665f7755d8dd45ca5d4d53c0c3dcce81f4
Bastien <X-death@ultimate-consoles.fr>
2024-08-29 10:39:56 +0200

    Preliminary main menu and some gfx
    Preliminary main menu and some gfx

---
+++ out/res/gfx.d
@@ -0,0 +1,3 @@
+out/res/gfx.o: res/gfx.res \
+D:/ProgMD/Projects/X-open-ED/res/ed_bottom.png \
+D:/ProgMD/Projects/X-open-ED/res/ed_title.png
---
+++ out/src/main.d
@@ -0,0 +1,21 @@
+out/src/main.o: src/main.c D:/ProgMD/inc/genesis.h D:/ProgMD/inc/types.h \
+ D:/ProgMD/inc/config.h D:/ProgMD/inc/asm.h D:/ProgMD/inc/sys.h \
+ D:/ProgMD/inc/sram.h D:/ProgMD/inc/mapper.h D:/ProgMD/inc/memory.h \
+ D:/ProgMD/inc/memory_base.h D:/ProgMD/inc/tools.h D:/ProgMD/inc/bmp.h \
+ D:/ProgMD/inc/maths.h D:/ProgMD/inc/vdp.h D:/ProgMD/inc/pal.h \
+ D:/ProgMD/inc/dma.h D:/ProgMD/inc/vdp_tile.h D:/ProgMD/inc/vdp_bg.h \
+ D:/ProgMD/inc/map.h D:/ProgMD/inc/pool.h D:/ProgMD/inc/object.h \
+ D:/ProgMD/inc/font.h D:/ProgMD/res/libres.h D:/ProgMD/inc/string.h \
+ D:/ProgMD/inc/tab_cnv.h D:/ProgMD/inc/maths3D.h D:/ProgMD/inc/vdp_spr.h \
+ D:/ProgMD/inc/vdp_pal.h D:/ProgMD/inc/vram.h D:/ProgMD/inc/sprite_eng.h \
+ D:/ProgMD/inc/sprite_eng_legacy.h D:/ProgMD/inc/z80_ctrl.h \
+ D:/ProgMD/inc/ym2612.h D:/ProgMD/inc/psg.h D:/ProgMD/inc/snd/sound.h \
+ D:/ProgMD/inc/snd/xgm.h D:/ProgMD/inc/snd/xgm2.h \
+ D:/ProgMD/inc/snd/smp_null.h D:/ProgMD/inc/snd/smp_null_dpcm.h \
+ D:/ProgMD/inc/snd/pcm/snd_pcm.h D:/ProgMD/inc/snd/pcm/snd_dpcm2.h \
+ D:/ProgMD/inc/snd/pcm/snd_pcm4.h D:/ProgMD/inc/joy.h \
+ D:/ProgMD/inc/timer.h D:/ProgMD/inc/task.h D:/ProgMD/inc/task_cst.h \
+ D:/ProgMD/inc/ext/flash-save/flash.h D:/ProgMD/inc/types.h \
+ D:/ProgMD/inc/ext/flash-save/saveman.h D:/ProgMD/inc/ext/console.h \
+ D:/ProgMD/inc/config.h D:/ProgMD/inc/string.h D:/ProgMD/inc/maths.h \
+ D:/ProgMD/inc/dma.h res/gfx.h
